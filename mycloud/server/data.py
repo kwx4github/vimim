3db5f79e2dbf3f29e3b94624bbc8c59226fee576
pan.shizhu <pan.shizhu@b50a9e10-f4e0-11dd-a295-0b7b0935bf94>
2010-01-25 08:32:23 +0000

    完善 mycloud 源代码中的版权信息
    git-svn-id: http://vimim.googlecode.com/svn@2691 b50a9e10-f4e0-11dd-a295-0b7b0935bf94

--- mycloud/server/data.py
+++ mycloud/server/data.py
@@ -1,6 +1,24 @@
 # coding=utf-8
 # create shuangpin->quanpin convertion table for most styles
-# Author: Pan, Shi Zhu
+"""
+    data for mycloud server
+
+    Copyright (C) 2010  Pan, Shi Zhu
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+"""
 
 import sys
 import string
